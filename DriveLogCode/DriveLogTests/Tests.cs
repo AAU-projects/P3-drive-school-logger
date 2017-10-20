5bd73bd90a88fb99378105b4a0e7e9f9e7d10d7c
Alechj <alec@hotmail.dk>
2017-10-20 18:38:49 +0200

    Added Nunit and CPRVerification Tests

---
+++ DriveLogCode/DriveLogTests/Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using DriveLogCode;
+
+namespace DriveLogTests
+{
+    public class Tests
+    {
+        [Test]
+        public void CPRValidation_InvalidInput_False()
+        {
+            bool result = RegisterVerification.CPRVerification("2010170035");
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void CPRValidation_ValidInput_True()
+        {
+            bool result = RegisterVerification.CPRVerification("2010170039");
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CPRValidation_ValidInputWithDash_True()
+        {
+            bool result = RegisterVerification.CPRVerification("201017-0039");
+
+            Assert.IsTrue(result);
+        }
+    }
+}
