24ea3bbf3f5d9f0411b61940936aa6f03db8abd6
Rasmus Egholm Nielsen <th3nielsen@hotmail.com>
2017-10-23 10:17:52 +0200

    ZipReader og JSONReader

---
+++ DriveLogCode/JSONReader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DriveLogCode
+{
+    public static class JSONReader
+    {
+        public static string GetCity(int zip)
+        {
+            WebClient client = new WebClient();
+            String downloadedString = client.DownloadString("https://dawa.aws.dk/postnumre?nr=8600");
+            RootObject r = JsonConvert.DeserializeObject<RootObject>(downloadedString);
+
+            return r.navn;
+        }
+    }
+}
+
+
