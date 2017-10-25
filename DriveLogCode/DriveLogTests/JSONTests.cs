4e134c92bfe5f58d3a7bd2e90454531fdf090284
Rasmus Egholm Nielsen <th3nielsen@hotmail.com>
2017-10-25 10:14:59 +0200

    JSONUnittesting

---
+++ DriveLogCode/DriveLogTests/JSONTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DriveLogCode;
+using NUnit.Framework;
+
+namespace DriveLogTests
+{
+    public class JSONTests
+    {
+        [TestCase(8700, ExpectedResult = "Horsens")]
+        [TestCase(6933, ExpectedResult = "Kibæk")]
+        [TestCase(9200, ExpectedResult = "Aalborg SV")]
+        [TestCase(4444, ExpectedResult = null)]
+        public string GetCity_recieveZipCode_returnsCityName(int zip)
+        {
+            return JSONReader.GetCity(zip);
+        }
+    }
+}
