c674d020fd9675adfa0fdcf944fa66d9a7df09e2
Rasmus Egholm Nielsen <th3nielsen@hotmail.com>
2017-10-30 14:21:05 +0100

    overviewTabv2

--- DriveLogGUI/overviewTab.cs
+++ DriveLogGUI/overviewTab.cs
@@ -16,5 +16,20 @@ namespace DriveLogGUI
         {
             InitializeComponent();
         }
+
+        private void progressBarPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void todaysNotePanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void theoreticalStatus_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
