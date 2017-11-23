bf79d566066206e329ac202b89ff9e4f33432b26
Alechj <alec@hotmail.dk>
2017-11-23 10:44:22 +0100

    Initial commit for Signature

---
+++ DriveLogGUI/SignatureEdit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DriveLogGUI
+{
+    public partial class SignatureEdit : Form
+    {
+        private Point _lastClick;
+        private Bitmap signatureImage = new Bitmap(540, 135);
+        public SignatureEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void topBarPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            _lastClick = e.Location;
+        }
+
+        private void topBarPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - _lastClick.X;
+                this.Top += e.Y - _lastClick.Y;
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
