6fbc0d5b1eed035f0b0b36b059d4a94c5695cc42
Alechj <alec@hotmail.dk>
2017-11-26 20:54:51 +0100

    Fixed the designer not working if inheriting from abstract control

--- DriveLogGUI/MenuTabs/overviewTab.cs
+++ DriveLogGUI/MenuTabs/overviewTab.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Forms;
 namespace DriveLogGUI.MenuTabs
 {
     internal delegate void SubPageNotification(UserControl origin);
+    [TypeDescriptionProvider(typeof(AbstractControlDescriptionProvider<OverviewTab, UserControl>))]
     public abstract class OverviewTab : UserControl
     {
         public virtual event EventHandler LogOutButtonClick;
